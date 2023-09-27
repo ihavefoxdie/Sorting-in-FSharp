6af0b6f0f23e832d19e99c2e4a7247ff22f2871d
Purpel Lizrd <vladislav.nekrasov123@gmail.com>
2023-09-27 20:33:03 +0300

    Add project files.

---
+++ BubbleSort/Program.fs
@@ -0,0 +1,75 @@
+﻿open System 
+
+
+let rec BubbleSort (numArray : int array) arraySize index (keepUp : bool) = 
+    let mutable keepGoing = keepUp
+    if index = (arraySize - 1) then 
+        if keepGoing then
+            BubbleSort numArray arraySize 0 false
+        keepGoing <- false
+    else if numArray.[index] > numArray.[index + 1] then 
+        keepGoing <- true 
+        let temp = numArray.[index] 
+        numArray.[index] <- numArray.[index + 1] 
+        numArray.[index + 1] <- temp 
+    if index < (arraySize - 1) then
+        BubbleSort numArray arraySize (index + 1) keepGoing 
+
+ ///////////////////////////////////////////////////////////////////////////////////////////////////////
+   
+let rec ShellThirdRecursion (numArray : int array) jindex size gap =
+    if jindex >= gap then
+        if numArray.[jindex] < numArray.[jindex - gap] then
+            let temp = numArray.[jindex]
+            numArray.[jindex] <- numArray.[jindex - gap]
+            numArray.[jindex - gap] <- temp;
+        ShellThirdRecursion numArray (jindex - gap) size gap
+
+let rec ShellSecondRecursion (numArray : int array) index size gap = 
+    if index <> size then
+        ShellThirdRecursion numArray index size gap
+        ShellSecondRecursion numArray (index + 1) size gap
+
+let rec ShellFirstRecursion (numArray : int array) gap size =
+    if gap > 0 then
+       ShellSecondRecursion numArray gap size gap
+       ShellFirstRecursion numArray (gap/2) size
+
+let ShellSort (numArray : int array) arraySize = 
+    ShellFirstRecursion numArray (arraySize/2) arraySize
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+[<EntryPoint>] 
+let main argv = 
+    printfn "Sorting in F#!" 
+
+
+
+    printfn "ShellSort" 
+    let arr = [|3; 5; 1; 8; 6; 10; 2; 234; 1; 1; 6; 100; 213|]
+    for i in 0 .. arr.Length - 1 do
+        printf "%d " arr.[i]
+    printfn ""
+
+    ShellSort arr arr.Length
+    for i in 0 .. arr.Length - 1 do
+        printf "%d " arr.[i]
+    printfn ""
+
+
+
+    printfn "BubbleSort" 
+    let arr = [|3; 5; 1; 8; 6; 10; 2; 234; 1; 1; 6; 100; 213|]
+    for i in 0 .. arr.Length - 1 do
+        printf "%d " arr.[i]
+    printfn ""
+
+    BubbleSort arr arr.Length 0 false
+    for i in 0 .. arr.Length - 1 do
+        printf "%d " arr.[i]
+    printfn ""
+
+
+
+    0
